498dc49c7380ea30cc85839ff2073af22a6168cf
Isaac <rodcel.isaac18@gmail.com>
2022-10-19 18:11:12 -0500

    Added background color and card color. Also make the header change title depending on where the user is.

--- app/controllers/legends_controller.rb
+++ app/controllers/legends_controller.rb
@@ -1,18 +1,20 @@
 class LegendsController < ApplicationController
   def main
-    @home_banner  = true
+    @main_header  = true
   end
 
   def index
     @legends = Legend.order(:name).includes(:role).all.page(params[:page]).per(6)
+    @index_header  = true
   end
 
   def show
     @legend = Legend.find(params[:id])
+    @show_header  = true
   end
 
   def about
-
+    @about_header = true
   end
 
   def search
